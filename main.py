46025a6563665166ba5e88e87a10d930bd40cf72
vivsoliveira <116001293+vivsoliveira@users.noreply.github.com>
2022-11-21 11:01:54 -0300

    tela de fundo + 1 espinho

---
+++ main.py
@@ -0,0 +1,34 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((480, 650))
+pygame.display.set_caption('Hello World!')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((194, 175, 181))  # Preenche com a cor branca
+
+    vertices = [(0, 0), (15, 15), (0, 30)]
+    pygame.draw.polygon(window,(0,0,0), vertices )
+
+    #(173,172,153)
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
