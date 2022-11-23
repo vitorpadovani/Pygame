9fe1d8ae1324e94553aa2a700815541c197b72e4
vivsoliveira <116001293+vivsoliveira@users.noreply.github.com>
2022-11-23 19:51:01 -0300

    classe bird

--- classes.py
+++ classes.py
@@ -17,12 +17,12 @@ HEIGHT_bird = 60
 
 # ----- Inicia assets
 background = pygame.image.load('assets/img/fundo.png').convert()
-bird_img1 = pygame.image.load('assets/img/bird.png').convert_alpha()
-bird_img1 = pygame.transform.scale(bird_img1, (HEIGHT_bird, WIDTH_bird))
-bird_img2 = pygame.image.load('assets/img/bird2.png').convert_alpha()
-bird_img2 = pygame.transform.scale(bird_img2, (HEIGHT_bird, WIDTH_bird))
+bird_img_dir = pygame.image.load('assets/img/bird.png').convert_alpha()
+bird_img_dir = pygame.transform.scale(bird_img_dir, (HEIGHT_bird, WIDTH_bird))
+bird_img_esq = pygame.image.load('assets/img/bird2.png').convert_alpha()
+bird_img_esq = pygame.transform.scale(bird_img_esq, (HEIGHT_bird, WIDTH_bird))
 bird_img = pygame.image.load('assets/img/bird.png').convert_alpha()
-bird_img = pygame.transform.scale(bird_img1, (HEIGHT_bird, WIDTH_bird))
+bird_img = pygame.transform.scale(bird_img, (HEIGHT_bird, WIDTH_bird))
 
 # ----- Inicia estruturas de dados
 game = True
@@ -50,36 +50,56 @@ class Bird(pygame.sprite.Sprite):
 
     def update(self):
         # Atualização da posição da nave
-        self.rect.x += self.speedx
+        self.rect.x += self.bird_speed_x
+        self.rect.y += self.bird_speed_y
 
         # Mantem dentro da tela
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
+
         if self.rect.left < 0:
             self.rect.left = 0
         
         if self.rect.x + self.rect.width >= 480:
-            bird_speed_x *= -1
-            bird_x += bird_speed_x
-            bird_img = bird_img2
-        if bird_x <= 0:
-            bird_speed_x *= -1
-            bird_x += bird_speed_x
-            bird_img = bird_img1
+            self.bird_speed_x *= -1
+            self.rect.x += self.bird_speed_x
+            bird_img = bird_img_esq
+
+        if self.rect.x <= 0:
+            self.bird_speed_x *= -1
+            self.rect.x += self.bird_speed_x
+            bird_img = bird_img_dir
+        if aplica_gravidade:
+            self.bird_speed_y += ACELERACAO
+            self.rect.y += self.bird_speed_y
+            self.rect.x += self.bird_speed_x
+
+            
+        #Quando bater na parede
+        if self.rect.x + WIDTH_bird >= 480:
+            self.bird_speed_x *= -1
+            self.rect.x += self.bird_speed_x
+            bird_img = bird_img_esq
+
+        elif self.bird_speed_x <= 0:
+            self.bird_speed_x *= -1
+            self.rect.x += self.bird_speed_x
+            bird_img = bird_img_dir
+        bird_x = self.rect.x
 
 
 #posição inicial do passaro
-bird_x = (WIDTH/2)
-bird_y = HEIGHT/2
+# bird_x = (WIDTH/2)
+# bird_y = HEIGHT/2
 
-# Gravidade aplicada a cada frame
+# Gravidade aplicada a cada frame 
 ACELERACAO = 0.8
 
 print('aperte espaço para pular com a bola')
 
-
-
-
+all_sprites = pygame.sprite.Group()
+player = Bird(bird_img)
+all_sprites.add(player)
 while game:
     clock.tick(FPS)
 
@@ -90,39 +110,39 @@ while game:
             game = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird_speed_y = -8
-                if bird_speed_x < 0:
-                    bird_speed_x = -8
-                else:
-                    bird_speed_x = 8
-                aplica_gravidade = True
+             if event.key == pygame.K_SPACE:
+                 bird_speed_y = -8
+                 if bird_speed_x < 0:
+                     bird_speed_x = -8
+                 else:
+                     bird_speed_x = 8
+        aplica_gravidade = True
 
     # ----- Atualiza estado do jogo
     #aplicando gravidade
-    if aplica_gravidade:
-        bird_speed_y += ACELERACAO
-        bird_y += bird_speed_y
-        bird_x += bird_speed_x
+    # if aplica_gravidade:
+    #     bird_speed_y += ACELERACAO
+    #     bird_y += bird_speed_y
+    #     bird_x += bird_speed_x
 
         
-    #Quando bater na parede
-    if bird_x + WIDTH_bird >= 480:
-        bird_speed_x *= -1
-        bird_x += bird_speed_x
-        bird_img = bird_img2
+    # #Quando bater na parede
+    # if bird_x + WIDTH_bird >= 480:
+    #     bird_speed_x *= -1
+    #     bird_x += bird_speed_x
+    #     bird_img = bird_img2
 
-    elif bird_x <= 0:
-        bird_speed_x *= -1
-        bird_x += bird_speed_x
-        bird_img = bird_img1
-
+    # elif bird_x <= 0:
+    #     bird_speed_x *= -1
+    #     bird_x += bird_speed_x
+    #     bird_img = bird_img1
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(background, (0, 0))
-    # Desenhando a bola na janela
-    window.blit(bird_img, (bird_x, bird_y))
+    # Desenhando o passaro na janela
+    all_sprites.draw(window)
+    #window.blit(player)
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
